10f76a2bae27aaf88fc8b8a723c4885da788b8f2
Lucas Moura <lucas.moura128@gmail.com>
2016-04-13 01:22:28 -0300

    Split data into training and test

--- mnist.py
+++ mnist.py
@@ -1,12 +1,19 @@
 import os
-import pandas as pd
 import pickle
+
+import numpy as np
+import pandas as pd
 
 
 DATA_FOLDER = 'data/'
 TRAINING_DATA_PATH = DATA_FOLDER + 'train.csv'
 
 TRAINING_DATA_PICKLE = DATA_FOLDER + 'train.pickle'
+
+
+def save_data(data):
+    with open(TRAINING_DATA_PICKLE, 'wa') as pkl_file:
+        pickle.dump(data, pkl_file)
 
 
 def load_data():
@@ -18,18 +25,36 @@ def load_data():
     return data
 
 
-def save_data(data):
-    with open(TRAINING_DATA_PICKLE, 'wa') as pkl_file:
-        pickle.dump(data, pkl_file)
+def shuffle_data(data):
+    return data.reindex(np.random.permutation(data.index))
+
+
+def split_data(data, split_ratio):
+    data = shuffle_data(data)
+
+    train_index = np.random.rand(len(data)) < split_ratio
+
+    return (data[train_index], data[~train_index])
 
 
 def main():
 
+    split_ratio = 0.9
+
     if os.path.isfile(TRAINING_DATA_PICKLE):
-        training_data = load_data()
+        print 'Loading data from pickle file...'
+        mnist_data = load_data()
     else:
-        training_data = pd.read_csv(TRAINING_DATA_PATH, header=0)
-        save_data(training_data)
+        print 'Reading csv...'
+        mnist_data = pd.read_csv(TRAINING_DATA_PATH, header=0)
+        save_data(mnist_data)
+
+    print 'Total data used: {}'.format(len(mnist_data))
+
+    training_data, test_data = split_data(mnist_data, split_ratio)
+
+    print 'Training data used: {}'.format(len(training_data))
+    print 'Test data used: {}'.format(len(test_data))
 
 
 if __name__ == '__main__':
